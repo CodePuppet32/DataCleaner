de6ea55411dc76597c4137b320ba9eff427552db
CodePuppet32 <harshgahlawat16@gmail.com>
2022-03-23 13:25:09 +0530

    implemented undo redo stack

--- Welcome.py
+++ Welcome.py
@@ -2,22 +2,34 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 import pandas as pd
+from functools import partial
 
 button_font = ('arial', 13)
+small_btn_font = ('arial', 10)
+
 default_button_options = {'activebackground': 'white', 'bg': 'RoyalBlue3', 'relief': 'groove',
                           'activeforeground': 'RoyalBlue3', 'width': '16',
                           'fg': 'white', 'font': button_font, 'bd': 2}
+
+another_button_options = {'activebackground': 'black', 'bg': 'springgreen2', 'relief': 'groove',
+                          'activeforeground': 'springgreen2', 'width': '9',
+                          'fg': 'black', 'font': small_btn_font, 'bd': 1}
 
 
 class MainWindow(tk.Tk):
     def __init__(self):
         super().__init__()
-
+        self.undo_stack = []
+        self.redo_stack = []
+        self.right_btn_list = None
+        self.left_btn_list = None
         self.get_column_window = None
         self.check_btn_vars = None
         self.numeric_df = None
         self.selected_columns = []
         self.df = pd.read_csv("C:/Users/rahul/Downloads/dataset.csv")
+        self.original_df = self.df.copy()
+        self.columns = self.df.columns
 
         screen_width = 1360
         screen_height = self.winfo_screenheight()
@@ -26,9 +38,13 @@ class MainWindow(tk.Tk):
         self.geometry("%dx%d" % (screen_width, screen_height))
         self.title('Happy Data Cleaning')
 
+        # label for showing rows x columns
+        self.row_col_display = Label(fg='gray7')
+        self.row_col_display.place(x=1360//2, y=5)
+
         # to show dataframe
         dataset_frame = LabelFrame(self, text='DataFrame', border=0)
-        dataset_frame.place(height=screen_height * 2 / 5, width=screen_width)
+        dataset_frame.place(y=20, height=screen_height * 2 / 5, width=screen_width)
         self.tree_view = ttk.Treeview(dataset_frame)
         self.tree_view.place(relheight=1, relwidth=1)
         treescrolly = Scrollbar(dataset_frame, orient="vertical",
@@ -45,9 +61,9 @@ class MainWindow(tk.Tk):
         manipulate_button_frame = LabelFrame(self, border=0, text='Manipulate DataFrame', pady=10)
         btn_padding_x = 8
         btn_padding_y = 12
-        Button(manipulate_button_frame, default_button_options, text='Delete NaN Rows', command=self.delete_columns) \
+        Button(manipulate_button_frame, default_button_options, text='Delete NaN Rows', command=self.remove_nans) \
             .grid(row=0, column=0, padx=btn_padding_x, pady=btn_padding_y)
-        Button(manipulate_button_frame, default_button_options, text='Delete Column', command=self.delete_column) \
+        Button(manipulate_button_frame, default_button_options, text='Delete Column', command=self.delete_cols) \
             .grid(row=0, column=1, padx=btn_padding_x, pady=btn_padding_y)
         Button(manipulate_button_frame, default_button_options, text='Change Datatype') \
             .grid(row=0, column=2, padx=btn_padding_x, pady=btn_padding_y)
@@ -71,87 +87,116 @@ class MainWindow(tk.Tk):
             .grid(row=1, column=3, padx=btn_padding_x, pady=btn_padding_y)
         Button(manipulate_button_frame, default_button_options, text='Dummy') \
             .grid(row=1, column=4, padx=btn_padding_x, pady=btn_padding_y)
-        Button(manipulate_button_frame, default_button_options, text='Dummy') \
+        Button(manipulate_button_frame, default_button_options, text='Redo') \
             .grid(row=1, column=5, padx=btn_padding_x, pady=btn_padding_y)
-        Button(manipulate_button_frame, default_button_options, text='Dummy') \
+        Button(manipulate_button_frame, default_button_options, text='Undo', command=self.undo) \
             .grid(row=1, column=6, padx=btn_padding_x, pady=btn_padding_y)
-        Button(manipulate_button_frame, default_button_options, text='Dummy') \
+        Button(manipulate_button_frame, default_button_options, text='Original Dataset', command=self.show_original) \
             .grid(row=1, column=7, padx=btn_padding_x, pady=btn_padding_y)
 
-        manipulate_button_frame.place(y=screen_height * 2 / 5, height=screen_height * 3 / 5, width=screen_width)
+        manipulate_button_frame.place(y=screen_height * 2 / 5 + 20, height=screen_height * 3 / 5, width=screen_width)
+
+    def save_state(self):
+        self.undo_stack.append(self.df.copy())
+
+    def redo(self):
+        if len(self.redo_stack):
+            self.df = self.redo_stack[-1]
+            
+
+    def undo(self):
+        if len(self.undo_stack):
+            self.df = self.undo_stack[-1]
+            self.redo_stack.append(self.undo_stack[-1])
+            self.undo_stack.pop()
+            self.show_dataset()
+
+    def show_original(self):
+        self.df = self.original_df
+        self.show_dataset()
+
+    def clear_all(self):
+        for item in self.tree_view.get_children():
+            self.tree_view.delete(item)
 
     def show_dataset(self):
+        self.clear_all()
+        num_rows, num_cols = self.df.shape
+        self.row_col_display['text'] = '{}x{}'.format(num_rows, num_cols)
         self.tree_view["column"] = list(self.df.columns)
         self.tree_view["show"] = "headings"
         for column in self.tree_view["columns"]:
             self.tree_view.heading(column, text=column)  # let the column heading = column name
-
         df_rows = self.df.to_numpy().tolist()  # turns the dataframe into a list of lists
         for row in df_rows:
             self.tree_view.insert("", "end", values=row)
 
-    def get_numeric_columns(self):
-        numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
-        self.numeric_df = self.df.select_dtypes(include=numerics)
-
-        get_column_window = Toplevel(self)
-        get_column_window.title('Select Columns')
-        get_column_window.geometry('320x220')
-
-    def delete_columns(self):
-        self.select_columns()
-        self.df.drop(self.selected_columns, inplace=True, axis=1)
-        self.show_dataset()
-
-    def select_columns(self):
-        columns = self.df.columns
-        total_columns = len(columns)
-        print(total_columns)
-        self.check_btn_vars = []
-        for i in range(total_columns):
-            self.check_btn_vars.append(IntVar())
+    def delete_cols(self):
+        self.selected_columns = []
 
         self.get_column_window = Toplevel(self)
         self.get_column_window.title('Select Columns')
-        self.get_column_window.geometry('380x520')
+        self.get_column_window.geometry('380x460')
 
-        top_frame = LabelFrame(self.get_column_window, height=400, width=380)
-        top_frame.pack()
+        total_columns = len(self.columns)
 
-        scrollbar = Scrollbar(top_frame, cursor='dot')
-        scrollbar.pack(side=RIGHT, fill=Y)
+        top_list_frame = LabelFrame(self.get_column_window, height=400, width=380)
 
-        check_list = Text(top_frame, width=380, padx=60)
-        check_list.pack()
+        scroll_bar = Scrollbar(top_list_frame)
+        scroll_bar.pack(side=RIGHT, fill=Y)
+
+        check_list = Text(top_list_frame)
+        check_list.pack(fill=X)
+
+        self.left_btn_list = [Button() for i in range(total_columns)]
+        self.right_btn_list = [Button() for i in range(total_columns)]
 
         for i in range(total_columns):
-            c_btn = Checkbutton(check_list, text=columns[i].upper(), variable=self.check_btn_vars[i], onvalue=1,
-                                offvalue=0, font=('Helvetica', 12), bg='white')
-            check_list.window_create('end', window=c_btn)
+            container = LabelFrame(check_list)
+            self.left_btn_list[i] = Button(container, another_button_options, text='Select', width=10, cursor='hand2',
+                                           command=partial(self.change_state, i, 0))
+            self.left_btn_list[i].pack(side=LEFT)
+            Label(container, text=self.columns[i].upper(), width=24).pack(side=LEFT)
+            self.right_btn_list[i] = Button(container, another_button_options, text='Remove', width=10, cursor='hand2',
+                                            state=DISABLED, command=partial(self.change_state, i, 1))
+            self.right_btn_list[i].pack(side=LEFT)
+            container.pack(fill=X)
+
+            check_list.window_create('end', window=container)
             check_list.insert('end', '\n')
 
-        check_list.config(yscrollcommand=scrollbar.set)
-        scrollbar.config(command=check_list.yview)
+        check_list.config(yscrollcommand=scroll_bar.set)
+        scroll_bar.config(command=check_list.yview)
         check_list.configure(state='disabled')
 
-        bottom_frame = LabelFrame(self.get_column_window)
+        top_list_frame.pack()
 
-        bottom_frame.pack(pady=30)
-        Button(bottom_frame, default_button_options, text='Done', command=self.get_selected_cols).pack(side=LEFT)
-        Button(bottom_frame, default_button_options, text='Close', command=self.get_column_window.destroy).pack(side=LEFT)
+        bottom_button_frame = LabelFrame(self.get_column_window)
+        Button(bottom_button_frame, default_button_options, text='Delete', bg='red3', command=partial(self.delete_cols_helper)).pack(side=LEFT)
+        bottom_button_frame.pack(pady=14)
 
-    def get_selected_cols(self):
+    def change_state(self, idx, right):
+        if right == 1:
+            self.selected_columns.remove(self.columns[idx])
+            self.left_btn_list[idx]['state'] = NORMAL
+            self.right_btn_list[idx]['state'] = DISABLED
+        else:
+            self.selected_columns.append(self.columns[idx])
+            self.left_btn_list[idx]['state'] = DISABLED
+            self.right_btn_list[idx]['state'] = NORMAL
+
+    def delete_cols_helper(self):
+        self.save_state()
+        self.df.drop(self.selected_columns, inplace=True, axis=1)
+        self.columns = self.df.columns
         self.selected_columns = []
-        columns = self.df.columns
+        self.get_column_window.destroy()
+        self.show_dataset()
 
-        for i, var in enumerate(self.check_btn_vars):
-            if var.get() == 1:
-                self.selected_columns.append(columns[i])
-
-        print(self.selected_columns)
-
-    def delete_column(self):
-        pass
+    def remove_nans(self):
+        self.save_state()
+        self.df.dropna(inplace=True)
+        self.show_dataset()
 
 
 main_window = MainWindow()
